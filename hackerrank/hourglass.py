9b43404d3577ae8aa38ac3735f11c2fea1fedc55
varun dev <mk.varun2002@gmail.com>
2022-01-27 22:09:47 +0530

    hour glass veralevel bro

--- hackerrank/hourglass.py
+++ hackerrank/hourglass.py
@@ -1,11 +1,27 @@
+def split(arr:list)->list:
+    mid = len(arr) // 2
+    return [arr[:mid], arr[mid:]]
+
 def hourglass(arr:list)->None:
-    for x in arr:
-        print(f"{x}")
-            
     
+    nums = [] 
     
+    for x in  range(len(arr)):
+        temp = arr[x]
+        nums.append([ temp[0:3], temp[1:4],temp[2:5] ,temp[3:6] ])
     
+    for x in nums:
+        print(x)
+        
+    print()
+    mid = []    
+    for x in range(1,5):
+        mid.append(nums[x][1])
+        print(mid[x-1])
     
+    print()
+    for x in range(len(nums)):
+        print(nums[x])
     
     
     
