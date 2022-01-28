eeb0441612374ef360abf3ab14adfa2118427ec2
varun dev <mk.varun2002@gmail.com>
2022-01-28 11:39:37 +0530

    hourglass done

--- hackerrank/hourglass.py
+++ hackerrank/hourglass.py
@@ -5,28 +5,61 @@ def split(arr:list)->list:
 def hourglass(arr:list)->None:
     
     nums = [] 
-    
+    # In matrix
     for x in  range(len(arr)):
         temp = arr[x]
         nums.append([ temp[0:3], temp[1:4],temp[2:5] ,temp[3:6] ])
     
     for x in nums:
         print(x)
-        
+  
     print()
-    mid = []    
+    mid =[]
     for x in range(1,5):
-        mid.append(nums[x][1])
-        print(mid[x-1])
-    
-    print()
-    for x in range(len(nums)):
-        print(nums[x])
-    
+        tem = []
+        for y in range(0,4):
+            tem.append(nums[x][y][1])
+        mid.append(tem)
+   
     
     
     
+    sum_of = []
+    for x in nums:
+        temp = []
+        for y in x:
+            temp.append(sum(y))
+        sum_of.append(temp)
+    print("\nsum of")
+    for x in sum_of:
+        print(x)    
     
+
+    max_out = []
+    A = []
+    B = []
+    
+    for x in range( len(sum_of)-2 ):
+        print(f" {x}  - { x+2}")
+        A.append( sum_of[x] )
+        B.append( sum_of[x+2] )
+
+
+    print("\nA and B")
+    for x in range( len(A) ):
+        print(f" {x} index {A[x]}  {B[x]}  "  )                
+    
+    print("\nOut")
+    for x in range( len(A) ):
+        temp = []
+        for y in range( len(B)):
+            # print(f" {A[x][y]}   {B[x][y]}"  )                
+            max_out.append(A[x][y] + B[x][y] + mid[x][y])
+            temp.append(A[x][y] + B[x][y] + mid[x][y])
+        print(temp)
+    
+    
+    print(f"\nMax {max(max_out)}")
     
     
     
